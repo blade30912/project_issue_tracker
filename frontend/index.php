ddb690faf5bd22f68331e3ed3fd83c3b5872b063
matthew bessette <blade30912@gmail.com>
2016-12-20 15:11:41 -0500

    added index

---
+++ frontend/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Issue Tracking App</title>
+	<link rel="stylesheet" href="/lib/bootstrap.min.css">
+	<link rel="stylesheet" href="/build/custom.css">
+	<script src="/lib/bootstrap.min.js"></script>
+	<script src="/lib/react.js"></script>
+	<script src="/build/app.js"></script>
+</head>
+<body>
+	
+</body>
+</html>
